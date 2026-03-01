75beb318a489ea17ee6ab9f73958335e4c622bf3
Daniel Francisco Lanzas <113854687+dflanzs@users.noreply.github.com>
2026-03-01 20:11:00 +0100

    Revert "Feat/back user dt os"

--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tfg.backend.GroupChat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupChatRepository extends JpaRepository<GroupChat, Long> {
-}
--- backend-springboot/src/main/java/com/tfg/backend/OneToOneChat/OneToOneChatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tfg.backend.OneToOneChat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OneToOneChatRepository extends JpaRepository<OneToOneChat, Long> {
-}
