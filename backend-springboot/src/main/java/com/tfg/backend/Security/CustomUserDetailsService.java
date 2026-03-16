ba0a3706bd8ede19819783d155e8b5ca72b4688e
dflanzs <dlanzaslarra@gmail.com>
2026-03-16 18:42:24 +0100

    feat: implement UserRole enum and enhance AuthorizationService for role-based access control

--- backend-springboot/src/main/java/com/tfg/backend/Security/CustomUserDetailsService.java
+++ backend-springboot/src/main/java/com/tfg/backend/Security/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package com.tfg.backend.Security;
 
 import com.tfg.backend.User.User;
 import com.tfg.backend.User.UserRepository;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -17,15 +20,22 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmailAndDeletedAtIsNull(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
+        // Srpingboot requires ROLE_ for preauthorization to work
+        List<GrantedAuthority> authorities = List.of(
+            new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                new ArrayList<>()
+                authorities
         );
     }
 }
