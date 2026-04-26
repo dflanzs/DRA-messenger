f0c8ffd7f89c42c1c37c3fc810ec82cec085ac39
dflanzs <dlanzaslarra@gmail.com>
2026-04-26 12:48:26 +0200

    feat: add custom sql queries

--- backend-springboot/src/main/java/com/tfg/backend/Notifications/NotificationRepository.java
+++ backend-springboot/src/main/java/com/tfg/backend/Notifications/NotificationRepository.java
@@ -1,9 +1,14 @@
 package com.tfg.backend.Notifications;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query("SELECT n FROM notification n WHERE n.user.id = :userId ORDER BY n.createdAt DESC")
     List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+
+    @Query("SELECT n FROM notification n WHERE n.user.id = :userId AND n.read = false ORDER BY n.createdAt DESC")
     List<Notification> findByUserIdAndReadFalseOrderByCreatedAtDesc(Long userId);
 }
