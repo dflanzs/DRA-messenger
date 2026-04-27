70f502e8efc97d3d4b1cbfb11a244e74bb9c74c8
dflanzs <dlanzaslarra@gmail.com>
2026-04-27 20:13:04 +0200

    feat: add enpoints for key directories

--- backend-springboot/src/main/java/com/tfg/backend/Cypher/SignalController.java
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/SignalController.java
@@ -2,6 +2,11 @@ package com.tfg.backend.Cypher;
 
 import com.tfg.backend.Cypher.dto.SignalBootstrapRequestDto;
 import com.tfg.backend.Cypher.dto.SignalBootstrapResponseDto;
+import com.tfg.backend.Cypher.dto.SignalBundleResponseDto;
+import com.tfg.backend.Cypher.dto.SignalRefillRequestDto;
+import com.tfg.backend.Cypher.dto.SignalRefillResponseDto;
+import com.tfg.backend.User.User;
+import com.tfg.backend.User.UserService;
 
 import jakarta.validation.Valid;
 
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,20 +26,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/signal")
 public class SignalController {
     private final SignalService signalService;
+    private final UserService userService;
 
-    public SignalController(SignalService signalService) {
+    public SignalController(SignalService signalService, UserService userService) {
         this.signalService = signalService;
+        this.userService = userService;
     }
 
 
-    @PostMapping("/keys/bootstrap/{userId}")
-    @PreAuthorize("@authorizationService.isSelf(authentication, #userId)")
+    @PostMapping("/keys/bootstrap")
     public SignalBootstrapResponseDto postKeysBootstrap(
-            @PathVariable Long userId,
             @Valid @RequestBody SignalBootstrapRequestDto request,
             Principal principal
     ) {
-        SignalBootstrapResponseDto response = signalService.replaceKeysBootstrap(userId, request);
+        User user = userService.getByEmail(principal.getName());
+        SignalBootstrapResponseDto response = signalService.replaceKeysBootstrap(user.getId(), request);
+        return response;
+    }
+
+    @PostMapping("/keys/one-time/refill")
+    public SignalRefillResponseDto postOneTimePreKeysRefill(
+            SignalRefillRequestDto request,
+            Principal principal
+    ) {
+        User user = userService.getByEmail(principal.getName());
+        SignalRefillResponseDto response = signalService.refillOneTimePreKeys(user.getId(), request);
+
+        return response;
+    }
+
+    @GetMapping("/users/{userId}/bundle")
+    @PreAuthorize("@authorizationService.isSelf(authentication, #userId)")
+    public SignalBundleResponseDto getUserBundle(
+            @PathVariable Long userId
+    ) {
+        SignalBundleResponseDto response = signalService.getUserBundle(userId);
         return response;
     }
 }
--- backend-springboot/src/main/java/com/tfg/backend/Cypher/SignalService.java
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/SignalService.java
@@ -1,9 +1,14 @@
 package com.tfg.backend.Cypher;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Base64.Decoder;
+import java.util.Set;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.tfg.backend.Cypher.Repositories.SignalAccountRepository;
 import com.tfg.backend.Cypher.Repositories.SignalKyberPreKeyRepository;
@@ -11,7 +16,10 @@ import com.tfg.backend.Cypher.Repositories.SignalOneTimePreKeyRepository;
 import com.tfg.backend.Cypher.Repositories.SignalSignedPreKeyRepository;
 import com.tfg.backend.Cypher.dto.SignalBootstrapRequestDto;
 import com.tfg.backend.Cypher.dto.SignalBootstrapResponseDto;
+import com.tfg.backend.Cypher.dto.SignalBundleResponseDto;
 import com.tfg.backend.Cypher.dto.SignalOneTimePreKeyDto;
+import com.tfg.backend.Cypher.dto.SignalRefillRequestDto;
+import com.tfg.backend.Cypher.dto.SignalRefillResponseDto;
 import com.tfg.backend.User.User;
 import com.tfg.backend.User.UserService;
 import com.tfg.backend.Cypher.Entity.SignalSignedPreKey;
@@ -54,6 +62,53 @@ public class SignalService {
             userAccount.SetUser(user);
         }
 
+        // Verify that all required fields are present in the request, if not throw an exception
+        if (request.getRegistrationId() == null ||
+            request.getIdentityKeyPublicB64() == null ||
+            request.getSignedPreKeyPublicB64() == null  ||
+            request.getSignedPreKeySignatureB64() == null ||
+            request.getKyberPreKeyPublicB64() == null  ||
+            request.getKyberPreKeySignatureB64() == null ||
+            request.getOneTimePreKeys() == null
+                ) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing required fields in the request");
+        }
+
+        int registrationId;
+        byte[] identityKeyPublicBytes;
+        byte[] signedPreKeyPublicBytes;
+        byte[] signedPreKeySignatureBytes;
+        byte[] kyberPreKeyPublicBytes;
+        byte[] kyberPreKeySignatureBytes;
+        List<SignalOneTimePreKeyDto> oneTimePreKeysPublicBytesList;        
+        Decoder decoder = java.util.Base64.getDecoder();
+
+        // Check if base64 keys are valid and decode them, if not throw an exception
+        try {
+            registrationId = request.getRegistrationId();
+
+            identityKeyPublicBytes = decoder.decode(request.getIdentityKeyPublicB64());
+            signedPreKeyPublicBytes = decoder.decode(request.getSignedPreKeyPublicB64());
+            signedPreKeySignatureBytes = decoder.decode(request.getSignedPreKeySignatureB64());
+            kyberPreKeyPublicBytes = decoder.decode(request.getKyberPreKeyPublicB64());
+            kyberPreKeySignatureBytes = decoder.decode(request.getKyberPreKeySignatureB64());
+
+            oneTimePreKeysPublicBytesList = request.getOneTimePreKeys();
+            for (SignalOneTimePreKeyDto signalOneTimePreKeyDto : oneTimePreKeysPublicBytesList) {
+                decoder.decode(signalOneTimePreKeyDto.getPublicKeyB64());
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(HttpStatus.BAD_REQUEST + " Invalid base64 keys");
+        }
+
+        // Check for duplicate pre-key IDs in one-time pre-keys, if found throw an exception
+        Set<Integer> oneTimePreKeyIds = new HashSet<>();
+        for (SignalOneTimePreKeyDto oneTimePreKey : oneTimePreKeysPublicBytesList) {
+            if (!oneTimePreKeyIds.add(oneTimePreKey.getPreKeyId())) {
+                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate pre-key ID in one-time pre-keys");
+            }
+        }
+
         // Retire existing keys
         SignalSignedPreKey userSignedPreKey = signalSignedPreKeyRepository.getByUserId(userId);
         if (userSignedPreKey != null) {
@@ -67,16 +122,15 @@ public class SignalService {
             signalKyberPreKeyRepository.save(userKyberPreKey);
         }
 
-        // Decode Base64 keys
-        Decoder decoder = java.util.Base64.getDecoder();
-
-        int registrationId = request.getRegistrationId();
-        byte[] identityKeyPublicBytes = decoder.decode(request.getIdentityKeyPublicB64());
-        byte[] signedPreKeyPublicBytes = decoder.decode(request.getSignedPreKeyPublicB64());
-        byte[] signedPreKeySignatureBytes = decoder.decode(request.getSignedPreKeySignatureB64());
-        byte[] kyberPreKeyPublicBytes = decoder.decode(request.getKyberPreKeyPublicB64());
-        byte[] kyberPreKeySignatureBytes = decoder.decode(request.getKyberPreKeySignatureB64());
-        List<SignalOneTimePreKeyDto> oneTimePreKeysPublicBytesList = request.getOneTimePreKeys();
+        for (SignalOneTimePreKey userOneTimePreKey : signalOneTimePreKeyRepository.getByUserId(userId)) {
+            userOneTimePreKey.SetConsumedAt(LocalDateTime.now());
+            signalOneTimePreKeyRepository.save(userOneTimePreKey);
+            
+            if (userOneTimePreKey != null) {
+                userOneTimePreKey.SetConsumedAt(LocalDateTime.now());
+                signalOneTimePreKeyRepository.save(userOneTimePreKey);
+            }
+        }
 
         // Create new keys
         SignalSignedPreKey newSignedPreKey = new SignalSignedPreKey();
@@ -98,6 +152,8 @@ public class SignalService {
         userAccount.SetActiveIdentityKeyPublic(identityKeyPublicBytes);
         signalAccountRepository.save(userAccount);
 
+        int oneTimePreKeysCount = 0;
+
         for (SignalOneTimePreKeyDto signalOneTimePreKeyDto : oneTimePreKeysPublicBytesList) {
             byte[] oneTimePreKeyPublicBytes = decoder.decode(signalOneTimePreKeyDto.getPublicKeyB64());
 
@@ -105,14 +161,104 @@ public class SignalService {
             newOneTimePreKey.SetPublicKey(oneTimePreKeyPublicBytes);
             newOneTimePreKey.SetUser(userAccount.GetUser());
             signalOneTimePreKeyRepository.save(newOneTimePreKey);
+
+            oneTimePreKeysCount++;
         }
 
         SignalBootstrapResponseDto response = new SignalBootstrapResponseDto(
-                newKyberPreKey.GetPreKeyId(),
                 newSignedPreKey.GetPreKeyId(),
-                oneTimePreKeysPublicBytesList.size()
+                newKyberPreKey.GetPreKeyId(),
+                oneTimePreKeysCount
                 );
 
         return response;
+    }
+
+    @Transactional
+    public SignalRefillResponseDto refillOneTimePreKeys(Long userId, SignalRefillRequestDto request){
+
+        // Get user account
+        SignalAccount userAccount = signalAccountRepository.getByUserId(userId);
+        if (userAccount == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User account not found");
+        }
+
+        // Check that one-time pre-keys are present in the request, if not throw an exception
+        List<SignalOneTimePreKeyDto> oneTimePreKeysPublicBytesList;
+
+        if (request.getOneTimePreKeys() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing one-time pre-keys in the request");
+        } else {
+            oneTimePreKeysPublicBytesList = request.getOneTimePreKeys();
+        }
+
+        // Check for duplicate pre-key IDs in one-time pre-keys, if found throw an exception
+        Set<Integer> oneTimePreKeyIds = new HashSet<>();
+        for (SignalOneTimePreKeyDto oneTimePreKey : oneTimePreKeysPublicBytesList) {
+            if (!oneTimePreKeyIds.add(oneTimePreKey.getPreKeyId())) {
+                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate pre-key ID in one-time pre-keys");
+            }
+        }
+
+        // Check if base64 keys are valid and decode them, if not throw an exception
+        Decoder decoder = java.util.Base64.getDecoder();
+        int oneTimePreKeysCount = 0;
+
+        for (SignalOneTimePreKeyDto oneTimePreKeyDto : oneTimePreKeysPublicBytesList) {
+            byte[] oneTimePreKeyPublicBytes;
+            try {
+                oneTimePreKeyPublicBytes = decoder.decode(oneTimePreKeyDto.getPublicKeyB64());
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException(HttpStatus.BAD_REQUEST + " Invalid base64 keys");
+            }
+
+            SignalOneTimePreKey newOneTimePreKey = new SignalOneTimePreKey();
+            newOneTimePreKey.SetPublicKey(oneTimePreKeyPublicBytes);
+            newOneTimePreKey.SetUser(userAccount.GetUser());
+            signalOneTimePreKeyRepository.save(newOneTimePreKey);
+            
+            oneTimePreKeysCount++;
+        }
+
+        return new SignalRefillResponseDto(oneTimePreKeysCount, oneTimePreKeysPublicBytesList.size() - oneTimePreKeysCount);
+    }
+
+    @Transactional
+    public SignalBundleResponseDto getUserBundle(Long userId) {
+        // Get user user account
+        SignalAccount userAccount = signalAccountRepository.getByUserId(userId);
+        if (userAccount == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User account not found");
+        }
+
+        // Check that user has active keys, if not throw an exception
+        if (userAccount.GetActiveIdentityKeyPublic() == null ||
+            userAccount.GetActiveSignedPreKey() == null ||
+            userAccount.GetActiveKyberPreKey() == null
+        ) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User keys not found");
+        }
+
+        SignalOneTimePreKey oneTimePreKey = signalOneTimePreKeyRepository.getUnconsumedPreKeyByUserId(userId);
+
+        String oneTimePreKeyString = java.util.Base64.getEncoder().encodeToString(oneTimePreKey.GetPublicKey());
+        String activeSignedPreKeyString = java.util.Base64.getEncoder().encodeToString(userAccount.GetActiveSignedPreKey().GetPublicKey());
+        String activeSignedPreKeySignatureString = java.util.Base64.getEncoder().encodeToString(userAccount.GetActiveSignedPreKey().GetSignature());
+        String activeIdentityKeyString = java.util.Base64.getEncoder().encodeToString(userAccount.GetActiveIdentityKeyPublic());
+        String activeKyberPreKeyString = java.util.Base64.getEncoder().encodeToString(userAccount.GetActiveKyberPreKey().GetPublicKey());
+        String activeKyberPreKeySignatureString = java.util.Base64.getEncoder().encodeToString(userAccount.GetActiveKyberPreKey().GetSignature();
+
+        return new SignalBundleResponseDto(
+                userAccount.GetRegistrationId(),
+                oneTimePreKey != null ? userAccount.GetActiveKyberPreKey().GetPreKeyId(): -1,
+                oneTimePreKeyString,
+                userAccount.GetActiveSignedPreKey().GetPreKeyId(),
+                activeSignedPreKeyString,
+                activeSignedPreKeySignatureString,
+                activeIdentityKeyString,
+                userAccount.GetActiveKyberPreKey().GetPreKeyId(),
+                activeKyberPreKeyString,
+                activeKyberPreKeySignatureString
+        );
     }
 }
