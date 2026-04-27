f6072b5db02d1ab463798480330e20d522bdddb3
dflanzs <dlanzaslarra@gmail.com>
2026-04-27 20:13:16 +0200

    fix: make fields final

--- backend-springboot/src/main/java/com/tfg/backend/Cypher/dto/SignalBootstrapResponseDto.java
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/dto/SignalBootstrapResponseDto.java
@@ -1,44 +1,17 @@
 package com.tfg.backend.Cypher.dto;
 
 public class SignalBootstrapResponseDto {
-    private Integer activeSignedPreKeyId;
-    private Integer activeKyberPreKeyId;
-    private Integer oneTimePreKeysStored;
-
-    public SignalBootstrapResponseDto() {
-    }
+    private final int activeSignedPreKeyId;
+    private final int activeKyberPreKeyId;
+    private final int oneTimePreKeysStored;
 
     public SignalBootstrapResponseDto(
-        Integer activeSignedPreKeyId,
-        Integer activeKyberPreKeyId,
-        Integer oneTimePreKeysStored
+        int activeSignedPreKeyId,
+        int activeKyberPreKeyId,
+        int oneTimePreKeysStored
     ) {
         this.activeSignedPreKeyId = activeSignedPreKeyId;
         this.activeKyberPreKeyId = activeKyberPreKeyId;
-        this.oneTimePreKeysStored = oneTimePreKeysStored;
-    }
-
-    public Integer getActiveSignedPreKeyId() {
-        return activeSignedPreKeyId;
-    }
-
-    public void setActiveSignedPreKeyId(Integer activeSignedPreKeyId) {
-        this.activeSignedPreKeyId = activeSignedPreKeyId;
-    }
-
-    public Integer getActiveKyberPreKeyId() {
-        return activeKyberPreKeyId;
-    }
-
-    public void setActiveKyberPreKeyId(Integer activeKyberPreKeyId) {
-        this.activeKyberPreKeyId = activeKyberPreKeyId;
-    }
-
-    public Integer getOneTimePreKeysStored() {
-        return oneTimePreKeysStored;
-    }
-
-    public void setOneTimePreKeysStored(Integer oneTimePreKeysStored) {
         this.oneTimePreKeysStored = oneTimePreKeysStored;
     }
 }
