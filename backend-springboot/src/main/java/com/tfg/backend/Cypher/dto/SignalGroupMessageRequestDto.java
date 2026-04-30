535aca4b3496dea04cf4817ba0249d3346c4b3f6
dflanzs <dlanzaslarra@gmail.com>
2026-04-30 17:51:15 +0200

    feat: dtos for sending group messages

---
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/dto/SignalGroupMessageRequestDto.java
@@ -0,0 +1,37 @@
+package com.tfg.backend.Cypher.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class SignalGroupMessageRequestDto {
+    @NotNull
+    private  Long recipientUserId;
+
+    // Equivalent to OneToOneChat
+    @NotNull
+    private  Long groupChatId;
+
+    @NotNull
+    private  Short cypherTextType;
+
+    @NotNull
+    private String cypherTextB64;
+
+    public SignalGroupMessageRequestDto() {
+    }
+
+    public Long getRecipientUserId() {
+        return recipientUserId;
+    }
+
+    public Long getGroupChatId() {
+        return groupChatId;
+    }
+
+    public Short getCypherTextType() {
+        return cypherTextType;
+    }
+
+    public String getCypherTextB64() {
+        return cypherTextB64;
+    }
+}
