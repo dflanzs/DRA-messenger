3b53f37aaf65933209332a6b21362195aec6047b
dflanzs <dlanzaslarra@gmail.com>
2026-04-30 17:52:45 +0200

    feat: is user on group chat

--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatRepository.java
+++ backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatRepository.java
@@ -3,4 +3,6 @@ package com.tfg.backend.GroupChat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupChatRepository extends JpaRepository<GroupChat, Long> {
+    
+    boolean existsByIdAndUser_Id(Long groupChatId, Long userId);
 }
