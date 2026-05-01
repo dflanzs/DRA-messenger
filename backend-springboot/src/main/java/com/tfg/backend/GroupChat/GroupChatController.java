8e24f8da44f582eea70a07311487dd0e4bd4d128
dflanzs <dlanzaslarra@gmail.com>
2026-05-01 11:23:01 +0200

    feat: add remaining endpoints to groupchats

--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatController.java
+++ backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatController.java
@@ -2,7 +2,10 @@ package com.tfg.backend.GroupChat;
 
 import com.tfg.backend.User.UserService;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -17,18 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GroupChatController {
 
     private final GroupChatRepository groupChatRepository;
+    private final GroupChatService groupcChatService;
 
     public GroupChatController(
             GroupChatService groupChatService,
-            GroupChatRepository groupChatRepository,
-            UserService userService 
+            GroupChatRepository groupChatRepository
     ) {
+        this.groupcChatService = groupChatService;
         this.groupChatRepository = groupChatRepository;
     }
 
-    @ResponseBody
-    @PreAuthorize("@authorizationService.isAdmin(authentication)")
     @GetMapping
+    @PreAuthorize("@authorizationService.isAdmin(authentication)")
     public List<GroupChat> list() {
         return groupChatRepository.findAll();
     }
@@ -42,5 +45,35 @@ public class GroupChatController {
         }
         groupChatRepository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/add/user/{userId}/group/{groupId}")    
+    public ResponseEntity<String> addUserToGroupChat(
+            @PathVariable Long userId,
+            @PathVariable Long groupId,
+            Principal principal
+    ) {
+        ResponseEntity<String> response = groupcChatService.addUserToGroupChat(userId, groupId, principal);
+        return response;
+    }
+
+    @ResponseBody
+    @GetMapping("/remove/user/{userId}/group/{groupId}")
+    public ResponseEntity<String> removeUserFromGroupChat(
+            @PathVariable Long userId,
+            @PathVariable Long groupId,
+            Principal principal
+    ) {
+        ResponseEntity<String> response = groupcChatService.removeUserFromGroupChat(userId, groupId, principal);
+        return response;
+    }
+
+    @GetMapping("/{groupId}/users")
+    public ResponseEntity<Set<Long>> getUsersInGroupChat(
+            @PathVariable Long groupId,
+            Principal principal
+    ) {
+        ResponseEntity<Set<Long>> response = groupcChatService.getUsersInGroupChat(groupId, principal);
+        return response;
     }
 }
--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatService.java
+++ backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatService.java
@@ -1,9 +1,16 @@
 package com.tfg.backend.GroupChat;
 
 import com.tfg.backend.SignalEnvelope.SignalEnvelopeRepository;
+import com.tfg.backend.User.User;
 import com.tfg.backend.User.UserRepository;
 import com.tfg.backend.User.UserService;
 
+import jakarta.transaction.Transactional;
+
+import java.security.Principal;
+import java.util.Set;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +19,83 @@ public class GroupChatService {
 
     private final GroupChatRepository groupChatRepository;
     private final UserRepository userRepository;
-    private final SimpMessagingTemplate messagingTemplate;
-    private final UserService userService;
-    private final SignalEnvelopeRepository signalEnvelopeRepository;
 
     public GroupChatService(
         GroupChatRepository groupChatRepository,
-        UserRepository userRepository,
-        SimpMessagingTemplate messagingTemplate,
-        UserService userService,
-        SignalEnvelopeRepository signalEnvelopeRepository
+        UserRepository userRepository
     ) {
         this.groupChatRepository = groupChatRepository;
         this.userRepository = userRepository;
-        this.messagingTemplate = messagingTemplate;
-        this.userService = userService;
-        this.signalEnvelopeRepository = signalEnvelopeRepository;
     }
 
+    @Transactional
+    public  ResponseEntity<String> addUserToGroupChat(Long userId, Long groupId, Principal principal) {
+        GroupChat groupChat = groupChatRepository.findById(groupId).orElse(null);
+        if (groupChat == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User currentUser = userRepository.findByEmail(principal.getName()).orElse(null);
+
+        if (!groupChat.getUserIds().contains(currentUser.getId())) {
+            return ResponseEntity.status(403).body("You are not a member of this group chat.");
+        }
+
+        User newUser = userRepository.findById(userId).orElse(null);
+        if (newUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (groupChat.getUserIds().stream().anyMatch(user -> newUser.getId().equals(userId))) {
+            return ResponseEntity.badRequest().body("User is already a member of the group chat.");
+        }
+
+        groupChat.getUserIds().add(userId);
+        groupChatRepository.save(groupChat);
+
+        return ResponseEntity.ok("User added to group chat successfully.");
+    }
+
+    @Transactional
+    public  ResponseEntity<String> removeUserFromGroupChat(Long userId, Long groupId, Principal principal) {
+        GroupChat groupChat = groupChatRepository.findById(groupId).orElse(null);
+        if (groupChat == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User currentUser = userRepository.findByEmail(principal.getName()).orElse(null);
+        if (!groupChat.getUserIds().contains(currentUser.getId())) {
+            return ResponseEntity.status(403).body("You are not a member of this group chat.");
+        }
+
+        User newUser = userRepository.findById(userId).orElse(null);
+        if (newUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!groupChat.getUserIds().stream().anyMatch(user -> newUser.getId().equals(userId))) {
+            return ResponseEntity.badRequest().body("User is not a member of the group chat.");
+        }
+
+        groupChat.getUserIds().remove(userId);
+        groupChatRepository.save(groupChat);
+
+        return ResponseEntity.ok("User removed from group chat successfully.");
+    }
+
+    @Transactional
+    public ResponseEntity<Set<Long>> getUsersInGroupChat(Long groupId, Principal principal){
+        GroupChat groupChat = groupChatRepository.findById(groupId).orElse(null);
+        if (groupChat == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User currentUser = userRepository.findByEmail(principal.getName()).orElse(null);
+        if (!groupChat.getUserIds().contains(currentUser.getId())) {
+            return ResponseEntity.status(403).build();
+        }
+
+        Set<Long> userIds = groupChat.getUserIds();
+        return ResponseEntity.ok(userIds);
+    }
 }
