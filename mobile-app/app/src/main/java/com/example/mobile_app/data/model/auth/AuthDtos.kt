a0eb2a631b765eb761b36374b9afd1995efbdb31
dflanzs <dlanzaslarra@gmail.com>
2026-05-01 16:55:04 +0200

    feat: authentication dto

---
+++ mobile-app/app/src/main/java/com/example/mobile_app/data/model/auth/AuthDtos.kt
@@ -0,0 +1,36 @@
+package com.example.mobile_app.data.model.auth
+
+data class RegisterRequestDto(
+    val name: String,
+    val email: String,
+    val password: String,
+)
+
+data class VerifyEmailDto(
+    val token: String,
+)
+
+data class LoginDto(
+    val email: String,
+    val password: String,
+)
+
+data class AuthResponseDto(
+    val token: String,
+    val user: UserResponseDto,
+)
+
+data class UserResponseDto(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val publicKey: String?,
+    val onlineStatus: Boolean,
+    val role: String,
+    val createdAt: String,
+)
+
+data class MessageResponseDto(
+    val message: String,
+)
+
