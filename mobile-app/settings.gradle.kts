4cd06fcd1d3a6871396b3b96c7926f7de39a5565
dflanzs <dlanzaslarra@gmail.com>
2026-05-05 14:58:00 +0200

    chore: add libsignal dependency

--- mobile-app/settings.gradle.kts
+++ mobile-app/settings.gradle.kts
@@ -19,6 +19,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "SignalBuildArtifacts"
+            url = uri("https://build-artifacts.signal.org/libraries/maven/")
+        }
     }
 }
 
