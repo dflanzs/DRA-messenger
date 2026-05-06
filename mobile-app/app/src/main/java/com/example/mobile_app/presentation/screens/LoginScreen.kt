8aebb7c95fcc2ab78d8dde042e70bb957d6baa16
dflanzs <dlanzaslarra@gmail.com>
2026-05-06 19:22:50 +0200

    chore: update texts

--- mobile-app/app/src/main/java/com/example/mobile_app/presentation/screens/LoginScreen.kt
+++ mobile-app/app/src/main/java/com/example/mobile_app/presentation/screens/LoginScreen.kt
@@ -47,7 +47,7 @@ fun LoginScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = "Signal App",
+            text = "Bienvenido",
             style = MaterialTheme.typography.headlineMedium,
         )
         Spacer(modifier = Modifier.height(8.dp))
