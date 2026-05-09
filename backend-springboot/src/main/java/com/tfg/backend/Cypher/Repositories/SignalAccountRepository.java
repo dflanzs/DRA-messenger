ce7c88ed0492c9df2387f73bc07c4bc0c5ffd528
dflanzs <dlanzaslarra@gmail.com>
2026-05-09 11:41:46 +0200

    fix: add lock to prevent concurrent writting failures

--- backend-springboot/src/main/java/com/tfg/backend/Cypher/Repositories/SignalAccountRepository.java
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/Repositories/SignalAccountRepository.java
@@ -1,12 +1,15 @@
 package com.tfg.backend.Cypher.Repositories;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Lock;
 
 import com.tfg.backend.Cypher.Entity.SignalAccount;
 
 public interface SignalAccountRepository extends JpaRepository<SignalAccount, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT s FROM SignalAccount s WHERE s.user.id = :userId")
     SignalAccount getByUserId(Long userId);
 }
