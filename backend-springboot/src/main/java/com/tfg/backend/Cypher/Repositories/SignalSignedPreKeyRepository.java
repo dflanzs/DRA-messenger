0a8a526fad8bcec26a6fa42c60db246bf3e8ba64
dflanzs <dlanzaslarra@gmail.com>
2026-05-15 19:36:54 +0200

    feat: add findByUserIdAndPreKeyId method to SignalKyberPreKeyRepository and SignalSignedPreKeyRepository

--- backend-springboot/src/main/java/com/tfg/backend/Cypher/Repositories/SignalSignedPreKeyRepository.java
+++ backend-springboot/src/main/java/com/tfg/backend/Cypher/Repositories/SignalSignedPreKeyRepository.java
@@ -9,4 +9,7 @@ public interface SignalSignedPreKeyRepository extends JpaRepository<SignalSigned
 
     @Query("SELECT s FROM SignalSignedPreKey s WHERE s.user.id = :userId")
     SignalSignedPreKey getByUserId(Long userId);
+
+    @Query("SELECT s FROM SignalSignedPreKey s WHERE s.user.id = :userId AND s.preKeyId = :preKeyId")
+    SignalSignedPreKey findByUserIdAndPreKeyId(Long userId, int preKeyId);
 }
