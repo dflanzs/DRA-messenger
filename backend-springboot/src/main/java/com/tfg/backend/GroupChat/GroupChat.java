9193ba977c0be14ab9d0c816014bba2d6e4e22ec
dflanzs <dlanzaslarra@gmail.com>
2026-05-17 20:44:56 +0200

    fix: wrong controller sintax, missing prepersist and creationtime

--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChat.java
+++ backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChat.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,6 +37,13 @@ public class GroupChat {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
 
     public Long getId() {
         return id;
--- backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatController.java
+++ backend-springboot/src/main/java/com/tfg/backend/GroupChat/GroupChatController.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 @RequestMapping("/api/group-chats")
 public class GroupChatController {
 
