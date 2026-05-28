bd8037b1396fd97dcc1dad347f2a927f0b5508b2
dflanzs <dlanzaslarra@gmail.com>
2026-05-28 17:29:27 +0200

    chore: remove unused import

--- backend-springboot/src/main/java/com/tfg/backend/User/UserRepository.java
+++ backend-springboot/src/main/java/com/tfg/backend/User/UserRepository.java
@@ -1,6 +1,5 @@
 package com.tfg.backend.User;
 
-import com.tfg.backend.Enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
     Optional<User> findByIdAndDeletedAtIsNull(Long id);
     List<User> findAllByDeletedAtIsNull();
-    List<User> findAllByDeletedAtIsNullAndRole(UserRole role);
     boolean existsByIdAndDeletedAtIsNull(Long id);
 }
